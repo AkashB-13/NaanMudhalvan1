5aec75f14e7f75bde58a05e1abcc2ad78f755654
AkashB-13 <akashbalasubramaniam13@gmail.com>
2025-06-11 19:41:30 +0530

    Update app6.py

--- app6.py
+++ app6.py
@@ -38,7 +38,7 @@ with row2[3]:
 
 row3=st.columns(4)
 with row2[0]:
-    st.image("3165.jpg", caption="THE FAIRY KING", use_container_width=True)
+    st.image("3419.jpg", caption="", use_container_width=True)
 
 @st.cache_data
 def load_data():
